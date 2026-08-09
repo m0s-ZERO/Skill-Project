1c950bae8df1254d44f7f14f22cc8ffb210cf6d7
m0s-ZERO <m4z59dwzvp@privaterelay.appleid.com>
2026-08-09 15:59:05 +0000

    Day9 JavaScriptでWebページを動かす

--- m0s_portfolio/script.js
+++ m0s_portfolio/script.js
@@ -1,7 +1,11 @@
 console.log("m0s portfolio");
 
-const button = document.querySelector(".works-button");
+const title = document.querySelector("#title");
+const message = document.querySelector("#message");
+const button = document.querySelector("#changeButton");
 
-button.addEventListener("click", function() {
-  alert("m0sのポートフォリオサイトへようこそ!");
+button.addEventListener("click", function(){
+  title.textContent = "ZERO";
+  message.textContent = "ボタンが押されました！";
+  title.classList.toggle("active");
 });
