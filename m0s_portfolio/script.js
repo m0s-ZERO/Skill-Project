552c71ad48846e4abde29a28cbd10736c0611050
m0s-ZERO <m4z59dwzvp@privaterelay.appleid.com>
2026-08-11 13:46:46 +0000

    Day11: JavaScriptでポートフォリオ作品一覧を動的に生成

--- m0s_portfolio/script.js
+++ m0s_portfolio/script.js
@@ -1,20 +1,22 @@
+// Day8
 console.log("m0s portfolio");
 
+// Day9
 const title = document.querySelector("#title");
 const message = document.querySelector("#message");
 const button = document.querySelector("#changeButton");
-
-button.addEventListener("click", function(){
+button.addEventListener("click", function () {
   title.textContent = "ZERO";
   message.textContent = "ボタンが押されました！";
   title.classList.toggle("active");
 });
 
+// Day10
 const nameInput = document.querySelector("#nameInput");
 const showButton = document.querySelector("#showButton");
 const nameMessage = document.querySelector("#nameMessage");
-showButton.addEventListener("click", function(){
-  if(nameInput.value === ""){
+showButton.addEventListener("click", function () {
+  if (nameInput.value === "") {
     nameMessage.classList.toggle("red-active");
     nameMessage.textContent = "名前を入力してください"
   } else {
@@ -22,3 +24,31 @@ showButton.addEventListener("click", function(){
     nameMessage.textContent = "ありがとうございます！" + nameInput.value + "さん";
   }
 });
+
+// Day11
+const works = [
+  {
+    title: "Skill Project",
+    category: "(Web)",
+  },
+  {
+    title: "Grow Value up",
+    category: "(Music)",
+  },
+{
+    title: "NRS T-shirt",
+    category: "(Design)",
+  }
+];
+console.log(works.length + "作品");
+for (let i = 0; i < works.length; i++) {
+  const workTitle = document.createElement("div");
+  const workCategory = document.createElement("div");
+  const project = document.querySelector(`#works-project${i + 1}`);
+  workTitle.textContent = works[i].title;
+  workCategory.textContent = works[i].category;
+  project.appendChild(workTitle);
+  project.appendChild(workCategory);
+  workTitle.classList.toggle("work-title");
+  workCategory.classList.toggle("work-category");
+}
