fa80ae8d8be1d3eb91d7b68fa0c1b72caf953f72
m0s-ZERO <m4z59dwzvp@privaterelay.appleid.com>
2026-08-13 13:40:03 +0000

    Day13: JavaScriptでクリックした作品の詳細情報を表示

--- m0s_portfolio/script.js
+++ m0s_portfolio/script.js
@@ -29,15 +29,15 @@ showButton.addEventListener("click", function () {
 const works = [
   {
     title: "Skill Project",
-    category: "(Web)",
+    category: "Web",
   },
   {
     title: "Grow Value up",
-    category: "(Music)",
+    category: "Music",
   },
   {
     title: "NRS T-shirt",
-    category: "(Design)",
+    category: "Design",
   }
 ];
 console.log(works.length + "作品");
@@ -46,17 +46,29 @@ for (let i = 0; i < works.length; i++) {
   const workCategory = document.createElement("div");
   const project = document.querySelector(`#works-project${i + 1}`);
   workTitle.textContent = works[i].title;
-  workCategory.textContent = works[i].category;
+  workCategory.textContent = "(" + works[i].category + ")";
   project.appendChild(workTitle);
   project.appendChild(workCategory);
   workTitle.classList.toggle("work-title");
   workCategory.classList.toggle("work-category");
 
-  // Day12：クリックイベント
-  project.addEventListener("click", function(event){
-  console.log("クリックされた要素：" + event.target.textContent);
-  console.log("タイトル：" + workTitle.textContent);
-  console.log("カテゴリー：" + workCategory.textContent);
-})
+  // Day12,Day13
+  project.addEventListener("click", function (event) {
+    const detail = document.querySelector("#work-detail");
+    const detailText = document.querySelector("#detail-text");
+    const detailTitle = document.createElement("div");
+    const detailCategory = document.createElement("div");
+    detailText.textContent = "クリックされたプロジェクト"
+    detailTitle.textContent = "タイトル：" + works[i].title;
+    detailCategory.textContent = "カテゴリー：" + works[i].category;
+    detail.innerHTML = "";
+    detail.appendChild(detailTitle);
+    detail.appendChild(detailCategory);
+    detailTitle.classList.toggle("detail-title");
+    detailCategory.classList.toggle("detail-category");
+    console.log("タイトル：" + workTitle.textContent);
+    console.log("カテゴリー：" + workCategory.textContent);
+    console.log("クリックされた要素：" + event.target.textContent);
+  })
 }
 
